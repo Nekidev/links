e6d7a9657b602dad4e897e6d9e4282cfbe296985
Rafael Bradley <rafabradleyrb@gmail.com>
2026-04-25 06:22:01 -0300

    Add some dynamism

--- static/js/script.js
+++ static/js/script.js
@@ -8,28 +8,35 @@ if (window.location.hostname == "localhost" || window.location.hostname == "127.
 
 const form = document.querySelector("#form");
 const input = document.querySelector("#url");
+const button = document.querySelector("#submit");
 const results = document.querySelector("#results");
 
 async function onSubmit(e) {
     e.preventDefault();
 
-    let long = input.value;
+    try {
+        button.innerText = "Shortening URL...";
 
-    let response = await fetch(`${apiHost}/links`, {
-        method: "POST",
-        body: JSON.stringify({ location: long }),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-    let data = await response.json();
-
-    if (!response.ok) {
-        alert("We couldn't generate your URL!");
-        return;
+        let long = input.value;
+    
+        let response = await fetch(`${apiHost}/links`, {
+            method: "POST",
+            body: JSON.stringify({ location: long }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        let data = await response.json();
+    
+        if (!response.ok) {
+            alert("We couldn't generate your URL!");
+            return;
+        }
+    
+        showResult(long, data.shortened);
+    } finally {
+        button.innerText = "Shorten URL";
     }
-
-    showResult(long, data.shortened);
 }
 
 function showResult(long, short) {
